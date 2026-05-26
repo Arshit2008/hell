77b7736675ebf9c905f7f04c2641ef0297131af5
Arshit <thalurarshit@gmail.com>
2026-05-26 11:18:03 +0530

    io

--- salary.py
+++ salary.py
@@ -38,6 +38,36 @@ with open('salary.pkl', 'rb') as f:
             break
         
 with open('salary.pkl', 'rb') as f:
-    ask = input('do you want to add data : (y/n): ')
-    ask2 = input
-        
+    ask = input('do you want to update data : (y/n): ')
+    if ask == 'y':
+        ans = 'y'
+    else:
+        ans = 'n'
+    while ans == 'y':
+        emp_id = input('Enter the employee ID to update: ')
+        updated = False
+        data_list = []
+        while True:
+            try:
+                data = pickle.load(f) 
+                if data['emp_id'] == emp_id:
+                    print('Employee found:')
+                    print(data) 
+                    name = input('Enter new name: ')
+                    salary = input('Enter new salary: ')
+                    data['name'] = name
+                    data['salary'] = salary
+                    updated = True
+                data_list.append(data)
+            except EOFError: 
+                if not updated:
+                    print('Employee ID not found.')
+                print('End of Program.')
+                break
+        with open('salary.pkl', 'wb') as f:
+            for item in data_list:
+                pickle.dump(item, f)
+        ans = input('Do you want to update more records? (y/n): ') 
+        if ans =='n':
+            break
+        print()   
